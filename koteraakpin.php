eb4f0144a2333b0a7f0d01265f27cb6b3db85e02
raka3275 <59763304+raka3275@users.noreply.github.com>
2020-06-23 23:09:28 +0700

    Update koteraakpin.php

--- koteraakpin.php
+++ koteraakpin.php
@@ -43,14 +43,14 @@ function change(){
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$message);
-        echo "\n".color("yellow","!] Claim voc PESANGOFOD2206");
+        echo "\n".color("yellow","!] Claim voc PESANGOFOOD2206");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
         sleep(1);
         }
         sleep(3);
-        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOD2206"}');
+        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
         $messageboba19 = fetch_value($boba19,'"message":"','"');
         if(strpos($boba19, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba19);
