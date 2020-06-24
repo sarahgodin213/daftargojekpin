9a6fb85ccd11f8e1e9733edc62cea6ba0b3b45d9
raka3275 <59763304+raka3275@users.noreply.github.com>
2020-06-24 12:33:09 +0700

    Update koteraakpin.php

--- koteraakpin.php
+++ koteraakpin.php
@@ -43,21 +43,21 @@ function change(){
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$message);
-        echo "\n".color("yellow","!] Claim voc COBAGOFOOD2206");
+        echo "\n".color("yellow","!] Claim voc WENDYS");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
         sleep(1);
         }
         sleep(3);
-        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
+        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"WENDYS"}');
         $messageboba19 = fetch_value($boba19,'"message":"','"');
         if(strpos($boba19, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba19);
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$messageboba19);
-        echo "\n".color("yellow","!] Claim voc PAKEGOFOOD2206");
+        echo "\n".color("yellow","!] Claim voc PESANGOFOOD2206");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
